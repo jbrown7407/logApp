ed07a36b026a08a4be2b4941fd811e4a65467221
jbrown7407 <jbrown7407@gmail.com>
2020-10-26 19:59:07 -0400

    UI

--- models/messages.js
+++ models/messages.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const messageSchema = new mongoose.Schema({
   message:   { type: String, required: true },
   user:  { type: String, required: true },
+  likes:  { type: Number },
   // level:  { type: String, required: true },
   // superUser: Boolean,
 }, {timestamps: true});
